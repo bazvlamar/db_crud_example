bb7b04a2fc03445764e63a62a53cace8391b4e12
bazvlamar <bazvlamar@gmail.com>
2025-10-22 12:27:36 +0300

    Update default.conf and index.php

---
+++ app/public/index.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+header('Content-Type: text/html; charset=UTF-8');
+echo "<h1>Hello, world 👋</h1>";
+echo "<p>PHP ".PHP_VERSION." via FPM + Nginx + MySQL (Docker Compose)</p>";
+?>
